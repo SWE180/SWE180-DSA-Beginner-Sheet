178de2e84968c7a09e57e36b8266b91daf6d4f24
subhamsubham6063 <sadhukhansubham588@gmail.com>
2025-09-07 11:42:26 +0530

    helloworld code

---
+++ Step 1 Basics checking in programing/Print Hello World/printHelloWorld.cpp
@@ -0,0 +1,18 @@
+//                                         ॐ
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+signed main()
+{
+  subham;
+ register ll testCase = 1;
+  cin>>testCase;
+  while (testCase--)
+  {
+    solve();
+  }
+  return 0;
+}
