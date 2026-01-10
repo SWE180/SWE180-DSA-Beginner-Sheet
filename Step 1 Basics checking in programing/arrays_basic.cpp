e76659b0c81f0164067661d9f06e3c5d83dedb62
Komal-180205 <vashisthkomal096@gmail.com>
2026-01-10 10:27:07 +0530

    Added basic array traversal program

---
+++ Step 1 Basics checking in programing/arrays_basic.cpp
@@ -0,0 +1,16 @@
+// Arrays - Basic traversal
+// Contributor: Komal
+// Time Complexity: O(n)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
